b953ac48734e55a6fe7b1892658bef6842aa790a
danitare868 <sincalexandrudaniel@gmail.com>
2024-07-24 22:28:54 +0300

    first commit

---
+++ src/main/java/org/example/entablebe/EntableBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.entablebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntableBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntableBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/entablebe/EntableBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.entablebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntableBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
